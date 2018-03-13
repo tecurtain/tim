1c66dff516554d7b4ee0a6e61ae09b98c298d513
Thomas Snow <thomas.snow@realestate.com.au>
2018-03-13 11:38:17 +1100

    Refactor All

--- javascript/index.js
+++ javascript/index.js
@@ -15,20 +15,15 @@ export function filter(inputArray, filterFunc) {
 
 export function all(inputArray, predicateFn) {
   var inputArrayLength = inputArray.length
-  var returnArray = []
   var currentIndex = 0
   while (currentIndex < inputArrayLength) {
     var currentElement = inputArray[currentIndex]
-    if(predicateFn(currentElement)) {
-      returnArray.push(currentElement)
+    if(!predicateFn(currentElement)) {
+      return false
     }
     currentIndex++
   }
-  if (returnArray.length === inputArrayLength) {
-    return true
-  } else {
-    return false
-  }
+  return true
 }
 
 export function any(inputArray, predicateFn) {
